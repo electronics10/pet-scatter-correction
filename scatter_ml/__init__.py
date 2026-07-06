020c59266a14d63b641a5fa2b316a0b7c37d0ae2
electronics10 <electronicstenio@gmail.com>
2026-07-06 17:19:56 +0800

    Add engine.py

--- scatter_ml/__init__.py
+++ scatter_ml/__init__.py
@@ -3,11 +3,13 @@ from .dataset import (SinogramWindowDataset, RunBatchSampler,
                       split_runs_by_recipe)
 from .model import UNet2p5D
 from .losses import mse, poisson_nll, LOSSES
+from .engine import fit, run_epoch, make_loader
 from .predict import predict_scatter, predict_scatter_from_ckpt, load_model
 
 __all__ = [
     "build_geometry", "MergeGeometry",
     "SinogramWindowDataset", "RunBatchSampler", "split_runs_by_recipe",
     "UNet2p5D", "mse", "poisson_nll", "LOSSES",
+    "fit", "run_epoch", "make_loader",
     "predict_scatter", "predict_scatter_from_ckpt", "load_model",
 ]
